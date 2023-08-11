78e69b9718eabaf4277c5f8854fa9f17dfb13fd4
b <walnuts@gmail.com>
2023-08-11 13:02:39 -0700

    make better

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,26 @@ export default function Home() {
 	const [players, setPlayers] = useState([]);
 	const [round, setRound] = useState(1);
 	const [showResetModal, setShowResetModal] = useState(false);
+	const [dots, setDots] = useState(".");
+	const { width, height } = useWindowSize();
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setDots((prevDots) => {
+				if (prevDots.length === 3) {
+					return "..."; // Display three dots
+				} else if (prevDots === "...") {
+					return "."; // Reset after a delay
+				} else {
+					return prevDots + ".";
+				}
+			});
+		}, 500); // Interval for adding dots
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, [dots]);
 
 	useEffect(() => {
 		const storedPlayers = localStorage.getItem("players");
@@ -50,12 +70,16 @@ export default function Home() {
 
 	const handleReset = () => {
 		setShowResetModal(true); // Show the reset modal
+
+		// Reset the players data
 	};
 
 	const handleConfirmReset = () => {
 		setShowResetModal(false); // Close the reset modal
 		setPlayers([]); // Reset the players data
 		setRound(1); // Reset the round
+		// start the dots again
+		setDots(".");
 	};
 
 	const handleCancelReset = () => {
@@ -64,15 +88,43 @@ export default function Home() {
 
 	return (
 		<div className="m-2">
-			<h1 className="text-3xl font-semibold text-white">
-				Golf Card Game Score Tracker
+			<h1 className="text-3xl font-semibold text-slate-200">
+				Golf Card Game{" "}
+				<span className=" font-semibold text-green-500">
+					{players.length < 2
+						? "- Add two or more players to begin" + dots
+						: ""}
+				</span>
 			</h1>
 
 			<div className="w-full flex justify-start space-x-4 items-start mt-4">
-				<div className="mx-2 bg-white p-4 rounded shadow space-y-2 inline-flex flex-col">
+				<div className="mx-2 p-4 rounded shadow space-y-2 inline-flex flex-col bg-slate-900">
+					<div className="bg-slate-700 text-white p-2 text-xl rounded flex justify-center">
+						{round ? (
+							<>
+								Round ‎
+								<span className="font-bold text-green-500">
+									{round}
+								</span>
+							</>
+						) : (
+							""
+						)}
+					</div>
+					<div id="totalScores">
+						<h2 className="font-semibold text-slate-200 text-3xl underline">
+							Score:
+						</h2>
+						{calculateCurrentTotalScores().map((total, index) => (
+							<p key={index} className="text-lg text-slate-200 ">
+								{players[index].name}: {total}
+							</p>
+						))}
+					</div>
+
 					<button
-						className={`px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 text-2xl ${
-							players.length < 2 ? "collapse" : "cursor-pointer"
+						className={`px-4 py-2 bg-blue-500 text-slate-900 rounded hover:bg-blue-600 text-2xl font-bold ${
+							players.length < 2 ? "hidden" : "cursor-pointer"
 						}`}
 						onClick={() => {
 							// check if all of the users got a score for the current round
@@ -83,9 +135,9 @@ export default function Home() {
 						Next Turn
 					</button>
 					<button
-						className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 text-2xl"
-						dd
-						Pla
+						className={`px-4 py-2 bg-green-500 text-slate-900 font-bold rounded hover:bg-green-600 text-2xl ${
+							round < 2 ? "cursor-pointer" : "hidden"
+						}`}
 						onClick={() => {
 							if (round === 1) {
 								handleAddPlayer(prompt("Enter player name"));
@@ -99,36 +151,25 @@ export default function Home() {
 					</button>
 					<button
 						onClick={handleReset}
-						className={`bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 text-2xl ${
-							players.length < 2 ? "collapse" : "cursor-pointer"
+						className={`bg-red-500 text-slate-900 px-4 py-2 rounded hover:bg-red-600 text-2xl font-bold ${
+							players.length < 2 ? "hidden" : "cursor-pointer"
 						}`}>
 						Reset Game
 					</button>
-
-					<div id="totalScores">
-						<h2 className="font-semibold text-slate-800 text-3xl underline">
-							Total Scores
-						</h2>
-						{calculateCurrentTotalScores().map((total, index) => (
-							<p key={index} className="text-lg text-slate-700">
-								{players[index].name}: {total}
-							</p>
-						))}
-					</div>
 				</div>
-				<div className="flex flex-col space-y-4">
+				<div className="flex flex-col">
 					{players.map((player, playerIndex) => (
 						<div
 							key={playerIndex}
-							className="bg-white p-4 rounded shadow">
-							<h3 className="text-lg font-semibold mb-2 text-slate-600">
+							className="p-2 rounded shadow bg-slate-900 mb-2">
+							<span className="text-lg font-semibold mb-2 text-green-600">
 								{player.name}
-							</h3>
-							<div className="flex space-x-1 break-words flex-wrap">
+							</span>
+							<div className="flex break-words flex-wrap space-x-1 justify-center items-center">
 								{Array.from({ length: round }, (_, index) => (
 									<input
 										key={index}
-										className="border rounded p-1 text-center text-slate-500 w-16 mb-1"
+										className="rounded shadow w-14 bg-slate-800 p-2"
 										type="number"
 										placeholder={`R${index + 1}`}
 										value={player.scores[index] || ""}
@@ -141,10 +182,10 @@ export default function Home() {
 									/>
 								))}
 							</div>
-							<p className="text-sm mt-2">
+							{/* <p className="text-sm mt-2">
 								Total Score:{" "}
 								{calculateTotalScore(player.scores)}
-							</p>
+							</p> */}
 						</div>
 					))}
 
