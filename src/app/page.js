f411fcf9fc09c5f61aa564ef48bf86763431d168
b <walnuts@gmail.com>
2023-08-11 13:03:59 -0700

    remove useWindowSize()

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,6 @@ export default function Home() {
 	const [round, setRound] = useState(1);
 	const [showResetModal, setShowResetModal] = useState(false);
 	const [dots, setDots] = useState(".");
-	const { width, height } = useWindowSize();
 
 	useEffect(() => {
 		const interval = setInterval(() => {
