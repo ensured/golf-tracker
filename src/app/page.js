00aa61ab25d9a9cd725f076aa1df22231de3adb1
ensured <walnuts@gmail.com>
2023-08-11 22:02:14 -0700

    tidy css

--- src/app/page.js
+++ src/app/page.js
@@ -94,152 +94,151 @@ export default function Home() {
 	};
 
 	return (
-		<div
-			className={`p-2 flex flex-col flex-wrap justify-center items-center  h-screen w-screen overflow-auto ${loading
-				? "bg-gradient-to-br from-slate-900 to-slate-600"
-				: "bg-gradient-to-br from-slate-900 to-slate-600"
-				}`}>
-			{loading ? (
-				<div
-					className="flex bg-slate-800  p-16 rounded border-2 border-slate-200	opacity-100
+		<div className="min-h-screen bg-slate-900">
+			<div
+				className={`p-2 flex flex-row flex-wrap justify-center items-center overflow-auto ${loading
+					? "bg-slate-900"
+					: "bg-slate-900"
+					}`}>
+				{loading ? (
+					<div
+						className="flex bg-slate-800  p-16 rounded border-2 border-slate-200	opacity-100
 					animate-pulse">
-					<span
-						className="text-white text-3xl
+						<span
+							className="text-white text-3xl
 				
 					
 					">
-						Loading...
-					</span>
-				</div>
-			) : (
-				<>
-					<h1 className="text-3xl font-semibold text-slate-200">
-						Golf Card Game{" "}
-					</h1>
-					<span className=" font-semibold text-green-400 text-sm ">
-						{players.length < 2
-							? "Please add two or more players to begin"
-							: ""}
-					</span>
-					<div className="flex justify-start items-start mt-4">
-						<div className="text-center mx-2 p-4 rounded shadow space-y-2 inline-flex flex-col bg-slate-900">
-							{players.length < 1 ? (
-								"" // If there are no players, do not show the round
-							) : round ? (
-								<div className="bg-slate-700 text-slate-50 p-1 text-xl rounded">
-									<span className="custom-animation">
-										Round {round}
-									</span>
-								</div>
-							) : (
-								""
-							)}
-
-							{players.length < 1 ? (
-								""
-							) : (
-								""
-							)}
-							<button
-								className={`px-4 py-2 bg-blue-500 text-slate-900 rounded hover:bg-blue-600 text-2xl font-bold ${players.length < 2
-									? "hidden"
-									: "cursor-pointer"
-									}`}
-								onClick={() => {
-									// check if all of the users got a score for the current round
-									if (isRoundComplete()) {
-										setRound((prevRound) => prevRound + 1);
-										// set new localStorage data
-										localStorage.setItem(
-											"players",
-											JSON.stringify(players)
-										);
-									}
-								}}>
-								Next Turn
-							</button>
-							<button
-								className={` bg-green-500 text-slate-900 font-bold rounded hover:bg-green-600 text-2xl ${round < 2 ? "cursor-pointer" : "hidden"
-									}`}
-								onClick={() => {
-									if (round === 1) {
-										handleAddPlayer(
-											prompt("Enter player name")
-										);
-									} else {
-										alert(
-											"Cannot add a new player while the game is in progress."
-										);
-									}
-								}}>
-								Add Player
-							</button>
-							<button
-								onClick={handleReset}
-								className={`bg-red-500 text-slate-900 px-4 py-2 rounded hover:bg-red-600 text-2xl font-bold ${players.length < 2
-									? "hidden"
-									: "cursor-pointer"
-									}`}>
-								Reset Game
-							</button>
-						</div>
-
-						<div className="flex flex-col items-start justify-center rounded shadow bg-slate-900">
-							{players.map((player, playerIndex) => (
-								<div
-									key={playerIndex}
-									className="p-2 rounded shadow text-lg m-4 border-b-2 border-slate-700">
-									<span className="text-lg font-semibold mb-2 text-slate-50 p-1 rounded">
-										{capitalizeFirstLetter(player.name)}{" "}
-										<span className="text-md mt-2 text-green-600">
-											Total Score:{" "}
-											{calculateTotalScore(player.scores)}
-										</span>
-									</span>
-									<div className="grid grid-cols-9 gap-2">
-										{Array.from(
-											{ length: round },
-											(_, index) => (
-												<input
-													key={index}
-													disabled={
-														round === 1 ? false : index + 1 === round
-															? false : true
-													}
-
-													className="p-1 rounded shadow bg-slate-800 text-center custom-animation"
-													type="text"
-													placeholder={`R${index + 1
-														}`}
-													value={
-														player.scores[index] ||
-														""
-													}
-													onChange={(e) =>
-														handleScoreChange(
-															playerIndex,
-															parseInt(
-																e.target.value
-															)
-														)
-													}
-												/>
-											)
-										)}
-									</div>
-
-								</div>
-							))}
-
-							<DraggableDialog // Render your Modal component
-								open={showResetModal}
-								handleClose={handleCancelReset}
-								handleConfirm={handleConfirmReset}
-							/>
-						</div>
+							Loading...
+						</span>
 					</div>
-				</>
-			)}
+				) : (
+					<>
+						<span className=" font-semibold text-green-400 text-sm ">
+							{players.length < 2
+								? "Please add two or more players to begin"
+								: ""}
+						</span>
+						<div className="flex justify-start items-start mt-4">
+							<div className="text-center mx-2 p-4 rounded shadow space-y-2 inline-flex flex-col bg-slate-900">
+								{players.length < 1 ? (
+									"" // If there are no players, do not show the round
+								) : round ? (
+									<div className="bg-slate-700 text-slate-50 p-1 text-xl rounded">
+										<span className="custom-animation">
+											Round {round}
+										</span>
+									</div>
+								) : (
+									""
+								)}
+
+								{players.length < 1 ? (
+									""
+								) : (
+									""
+								)}
+								<button
+									className={`px-4 py-2 bg-blue-500 text-slate-900 rounded hover:bg-blue-600 text-2xl font-bold ${players.length < 2
+										? "hidden"
+										: "cursor-pointer"
+										}`}
+									onClick={() => {
+										// check if all of the users got a score for the current round
+										if (isRoundComplete()) {
+											setRound((prevRound) => prevRound + 1);
+											// set new localStorage data
+											localStorage.setItem(
+												"players",
+												JSON.stringify(players)
+											);
+										}
+									}}>
+									Next Turn
+								</button>
+								<button
+									className={`p-3 bg-green-500 text-slate-900 font-bold rounded hover:bg-green-600 text-2xl ${round < 2 ? "cursor-pointer" : "hidden"
+										}`}
+									onClick={() => {
+										if (round === 1) {
+											handleAddPlayer(
+												prompt("Enter player name")
+											);
+										} else {
+											alert(
+												"Cannot add a new player while the game is in progress."
+											);
+										}
+									}}>
+									Add Player
+								</button>
+								<button
+									onClick={handleReset}
+									className={`bg-red-500 text-slate-900 px-4 py-2 rounded hover:bg-red-600 text-2xl font-bold ${players.length < 2
+										? "hidden"
+										: "cursor-pointer"
+										}`}>
+									Reset Game
+								</button>
+							</div>
+
+							<div className="flex flex-col items-start justify-center rounded shadow bg-slate-900">
+								{players.map((player, playerIndex) => (
+									<div
+										key={playerIndex}
+										className="p-2 rounded shadow text-lg m-4 border-b-2 border-slate-700">
+										<span className="text-lg font-semibold mb-2 text-slate-50 p-1 rounded">
+											{capitalizeFirstLetter(player.name)}{" "}
+											<span className="text-md mt-2 text-green-600">
+												Total Score:{" "}
+												{calculateTotalScore(player.scores)}
+											</span>
+										</span>
+										<div className="grid grid-cols-9 gap-2">
+											{Array.from(
+												{ length: round },
+												(_, index) => (
+													<input
+														key={index}
+														disabled={
+															round === 1 ? false : index + 1 === round
+																? false : true
+														}
+
+														className="p-1 rounded shadow bg-slate-800 text-center custom-animation"
+														type="text"
+														placeholder={`R${index + 1
+															}`}
+														value={
+															player.scores[index] ||
+															""
+														}
+														onChange={(e) =>
+															handleScoreChange(
+																playerIndex,
+																parseInt(
+																	e.target.value
+																)
+															)
+														}
+													/>
+												)
+											)}
+										</div>
+
+									</div>
+								))}
+
+								<DraggableDialog // Render your Modal component
+									open={showResetModal}
+									handleClose={handleCancelReset}
+									handleConfirm={handleConfirmReset}
+								/>
+							</div>
+						</div>
+					</>
+				)}
+			</div>
 		</div>
 	);
 }
